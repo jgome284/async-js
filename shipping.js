4501cf68c64b1bbe81303ab703a4a07b146f0b10
Jose M. Gomez <gomezjosem@live.com>
2024-03-01 12:38:13 -0500

    created shipment processing module

---
+++ shipping.js
@@ -0,0 +1,19 @@
+/*
+This function generates a random number to serve as a "tracking number" on the shipping label.
+In real life this wouldn't be a random number...
+ */
+
+function generateTrackingNumber() {
+    return Math.floor(Math.random() * 1000000);
+  }
+
+const shipOrder = (responseArray) => {
+  const order = responseArray[0];
+  console.log("Generating shipping label.")
+  const trackingNum =generateTrackingNumber();
+  return new Promise ((resolve, reject) => {
+   setTimeout(()=> {  
+     resolve(`The order has been shipped. The tracking number is: ${trackingNum}.`);
+}, generateRandomDelay());
+ });
+};
