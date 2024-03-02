8f8a82c74db67d194ff432f256f81cf356bb3f33
Jose M. Gomez <gomezjosem@live.com>
2024-03-01 22:50:16 -0500

    added promisified readfile utility

--- utils.js
+++ utils.js
@@ -11,3 +11,15 @@ function randomSuccess(pct) {
     return (Math.random() > pct);
 }
 
+const fs = require('fs');
+
+// Below we create a function for reading files that returns a promise. We converted the fs.readfile() function which uses callbacks. Many of the asynchronous functions you'll encounter already return promises, so this extra step is seldom necessary. 
+const promisifiedReadfile = (file, encoding) => 
+  new Promise((resolve, reject) => {
+    fs.readFile(file, encoding, (err, text) => {
+			if (err) {
+				return reject(err.message);
+      }
+        resolve(text);
+      });
+});
